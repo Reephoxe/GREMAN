593c84f41701fdd7c0c3048a7e563a1ff510f9d6
RomainGames <romainromanwattpad@gmail.com>
2025-01-22 09:42:40 +0100

    Ajout de MainApplication, fenêtre coupé en 3

---
+++ src/main/java/polytech/univtours/greman/MainController.java
@@ -0,0 +1,21 @@
+package polytech.univtours.greman;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
+public class MainController {
+
+    @FXML
+    private HBox topBox;
+
+    @FXML
+    private HBox centerBox;
+
+    @FXML
+    private HBox bottomBox;
+
+    @FXML
+    public void initialize() {
+        // Additional initialization if needed
+    }
+}
