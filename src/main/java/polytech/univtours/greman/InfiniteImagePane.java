59978ebfed6e69e85bae987add3dcfe08b8b5742
RomainGames <romainromanwattpad@gmail.com>
2025-02-12 00:33:27 +0100

    Ajout de CircuitBuilder, créateur d'image de circuit. Modification de InfiniteImagePane, pour n'afficher qu'une fois image.png

--- src/main/java/polytech/univtours/greman/InfiniteImagePane.java
+++ src/main/java/polytech/univtours/greman/InfiniteImagePane.java
@@ -11,22 +11,11 @@ public class InfiniteImagePane extends Pane {
 
     public InfiniteImagePane() {
         // Chargez l'image (assurez-vous que le chemin soit correct)
-        Image image = new Image("file:C:\\Users\\romai\\IdeaProjects\\GREMAN\\src\\main\\resources\\image.png");
-        // Définissez ici le facteur de réduction, par exemple 0.5 pour 50%
-        double reductionFactor = 0.1;
-        // Utilisez la largeur originale comme taille de tuile (ou adaptez selon vos besoins)
+        Image image = new Image("file:src\\main\\resources\\image.png");
         int tileSize = (int) image.getWidth();
-        for (int x = -5; x < 5; x++) {
-            for (int y = -5; y < 5; y++) {
-                ImageView imageView = new ImageView(image);
-                imageView.setFitWidth(tileSize * reductionFactor);
-                imageView.setFitHeight(tileSize * reductionFactor);
-                imageView.setPreserveRatio(true);
-                imageView.setX(x * tileSize * reductionFactor);
-                imageView.setY(y * tileSize * reductionFactor);
-                getChildren().add(imageView);
-            }
-        }
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        getChildren().add(imageView);
 
         // Gestion du drag (déplacement de la vue)
         setOnMousePressed(e -> {
