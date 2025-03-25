fc3ac08420da5815dbd32c57e893ad04f1a7aa89
liliangsqr <lilian.gsqr@gmail.com>
2025-03-25 19:10:22 +0100

    Apres merge
    merger /!\ pop up de maxime a mettre ailleurs que dans hello controller

--- src/main/java/polytech/univtours/greman/HelloController.java
+++ src/main/java/polytech/univtours/greman/HelloController.java
@@ -48,7 +48,7 @@ public class HelloController {
         Lecture_Creation_Circuit _lecture = new Lecture_Creation_Circuit();
         _lecture._lectureCSV();
 
-        if (file != null) {
+        /*if (file != null) {
             String fileName = file.getName();
             if (fileName.endsWith(".s1p") || fileName.endsWith(".s2p")) {
                 // Si le fichier est de type s1p ou s2p, appelle la méthode sparameter
@@ -77,7 +77,7 @@ public class HelloController {
         System.out.println("Nb bobines: " + inputDialogController.getNombreBobines());
         System.out.println("Nb condensateurs: " + inputDialogController.getNombreCondensateurs());
         System.out.println("Pondération bobines: " + inputDialogController.getPonderationBobines());
-        System.out.println("Pondération condensateurs: " + inputDialogController.getPonderationCondensateurs());
+        System.out.println("Pondération condensateurs: " + inputDialogController.getPonderationCondensateurs());*/
 
     }
 }
